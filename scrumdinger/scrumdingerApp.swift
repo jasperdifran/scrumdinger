ff12c7d604c36f9fe8d55d08db48486a120eee8c
jasperdifran <68628968+jasperdifran@users.noreply.github.com>
2021-08-04 14:07:57 +1000

    Initial Commit

---
+++ scrumdinger/scrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  scrumdingerApp.swift
+//  scrumdinger
+//
+//  Created by Jasper Di Francesco on 4/8/21.
+//
+
+import SwiftUI
+
+@main
+struct scrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
